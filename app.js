dc22b7bba70c1936d418958065f9b9ba9b7d4953
Pelumi Raphael <pelumiogunsina7@gmail.com>
2024-12-12 16:26:48 +0100

    backend for production

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use("/api/test", testRouter);
 
 
 const __dirname = path.resolve(); 
-app.use(express.static(path.join(__dirname, "client", "build"))); 
+app.use(express.static(path.join(__dirname, "client", "dist"))); 
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
